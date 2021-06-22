b43e4bbb8add1893f60c7befbdf74fe1e858410b
89749 <tyousaryuu@163.com>
2021-06-23 01:23:47 +0800

    20210623

--- 疯狂Java/第1课 数组与内存控制/tt/ss/rr/ArrayTest0.java
+++ 疯狂Java/第1课 数组与内存控制/tt/ss/rr/ArrayTest0.java
@@ -5,7 +5,7 @@ public class ArrayTest0 {
 	public static void main(String[] args) {
 		int[] pos = new int[5];
 		for (int i = 0; i < pos.length; i++) {
-			pos[i] = (i+1)*2;//2 4 6 8 10 
+			pos[i] = (i+1)*2;//2 4 6 8 10
 		}
 		int a = pos[1];
 		int b = 20;
